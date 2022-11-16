6d857d1c9a27d2494adf16df0fa6a2dfbed32777
mahesh <vmc6300@gmail.com>
2022-11-16 12:00:00 +0530

    commited

---
+++ src/main/java/mysampleproject/ExamMavenProject/App.java
@@ -0,0 +1,13 @@
+package mysampleproject.ExamMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam,created project is ExamMavenProject" );
+    }
+}
